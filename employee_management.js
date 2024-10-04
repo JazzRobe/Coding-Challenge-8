a40093e06210fb138feed9a9ec35e26c3d40a5a8
Jazzmyne Roberson <jroberson0927@gmail.com>
2024-10-04 13:41:35 -0400

    Create Department class to manage employees and calculate salary

--- employee_management.js
+++ employee_management.js
@@ -1,13 +1,32 @@
 //task 1: create an employee class
 
-class Department {
+class Employee {
     constructor(name, salary, position, department) {
         this.name = name;
         this.salary = salary;
         this.position = position;
         this.department = department;
     }
+
     getDetails() {
         console.log(`${this.name} works as a ${this.position} earning $${this.salary}.`);
+    }
+}
+
+
+//task 2: create a department class
+
+class Department {
+    constructor(name, employees) {
+        this.name = name;
+        this.employees = [employees];
+    }
+
+    addEmployee(employee) {
+        employees.push(new Employee());
+    }
+
+    getDepartmentSalary() {
+        return this.employees.reduce((total, employee) + total + employee.salary, 0);
     }
 }
